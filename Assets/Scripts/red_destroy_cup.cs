a1a6936bcf1ab8482cac5611435af9c6c9a683ae
Toastedvenom <tilakkamkar@gmail.com>
2023-01-16 19:02:49 +0100

    merge conflicts resolved

--- Assets/Scripts/red_destroy_cup.cs
+++ Assets/Scripts/red_destroy_cup.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class red_destroy_cup : MonoBehaviour
 {
     void OnTriggerEnter(Collider other){
-<<<<<<< HEAD
         if(other.CompareTag("red_destroy_cup"))
         {
             Destroy(other.gameObject);
             GameManager.Instance.Player1Scored();
-=======
-        if(other.CompareTag("red_destroy_cup")){
-            Destroy(other.gameObject);
->>>>>>> e75eeec0923cc1b82a7360ffd31392f34fd5b33b
         }
     }
 }
